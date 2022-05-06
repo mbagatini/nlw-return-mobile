4dcb75430c9a7e2f63e551132ff2253860b44b2e
Morgana <morganabagatini@gmail.com>
2022-05-05 22:20:16 -0300

    Modal

---
+++ src/components/Widget/index.tsx
@@ -0,0 +1,39 @@
+import BottomSheet from "@gorhom/bottom-sheet";
+import { ChatTeardropDots } from "phosphor-react-native";
+import React, { useRef } from "react";
+import { TouchableOpacity, Text } from "react-native";
+import { gestureHandlerRootHOC } from "react-native-gesture-handler";
+
+import { theme } from "../../theme";
+import { styles } from "./styles";
+
+function WidgetComponent() {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  const handleOpenBottomSheet = () => {
+    bottomSheetRef.current?.expand();
+  };
+
+  return (
+    <>
+      <TouchableOpacity style={styles.button} onPress={handleOpenBottomSheet}>
+        <ChatTeardropDots
+          size={24}
+          color={theme.colors.text_on_brand_color}
+          weight={"bold"}
+        />
+      </TouchableOpacity>
+
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
+      >
+        <Text>oie</Text>
+      </BottomSheet>
+    </>
+  );
+}
+
+export const Widget = gestureHandlerRootHOC(WidgetComponent);
