9ea609f5dd530f3afd9f425421ae3258bce2e587
Morgana <morganabagatini@gmail.com>
2022-05-05 21:05:29 -0300

    fontes

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
+
 import { Widget } from "./src/components/Widget";
 import { theme } from "./src/theme";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  // Aguarda na tela de splash enquanto as fontes estão sendo carregadas
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={{ flex: 1, backgroundColor: theme.colors.background }}>
       <Widget />
