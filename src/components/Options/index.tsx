e3456b75c4b387b6fedcb7a35bd598f6d9387d53
Morgana <morganabagatini@gmail.com>
2022-05-08 23:56:49 -0300

    controle dos tipos

--- src/components/Options/index.tsx
+++ src/components/Options/index.tsx
@@ -5,14 +5,24 @@ import { Option } from "../Option";
 import { Copyright } from "../Copyright";
 import { styles } from "./styles";
 import { feedbackTypes } from "../../utils/feedbackTypes";
+import { FeedbackType } from "../Widget";
 
-export function Options() {
+interface OptionsProps {
+  onChange: (type: FeedbackType) => void;
+}
+
+export function Options({ onChange }: OptionsProps) {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Deixe seu feedback</Text>
       <View style={styles.options}>
         {Object.entries(feedbackTypes).map(([key, value]) => (
-          <Option key={key} image={value.image} title={value.title} />
+          <Option
+            key={key}
+            image={value.image}
+            title={value.title}
+            onPress={() => onChange(key as FeedbackType)}
+          />
         ))}
       </View>
       <Copyright />
